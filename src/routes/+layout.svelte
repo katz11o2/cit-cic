88618ac2dbf1726ba53cb22a1a21db4129806e67
katz11o2 <Thiruvenkat.er@gmail.com>
2025-07-08 21:24:10 +0530

    Initial push for Vercel deployment

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Header from '../components/Header.svelte';
+  import Footer from '../components/Footer.svelte';
+</script>
+
+<div class="app-wrapper">
+  <Header />
+
+  <main class="main-content">
+    <slot />
+  </main>
+
+  <Footer />
+</div>
+
+<style>
+  .app-wrapper {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    width: 100%;
+  }
+
+  .main-content {
+    flex-grow: 1;
+  }
+</style>
