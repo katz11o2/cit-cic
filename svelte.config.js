3c3be17810fba8471af3bf99ba7c6e506788fbf7
katz11o2 <Thiruvenkat.er@gmail.com>
2025-07-08 21:26:45 +0530

    Setup SvelteKit with Vercel adapter for auto deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter()
   }
 };
+
+export default config;
